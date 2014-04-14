076a61b8af14222edc37595dc6ee6b71f1c0862c
Petr Kočí <petr.koci@gmail.com>
2014-04-14 13:40:52 +0200

    oscrapované posty

--- strany.R
+++ strany.R
@@ -1,3 +1,17 @@
-strany  <- c("snked", "narodnidemokracie", "kducsl", "StranaZdravehoRozumu", "top09cz", "stranales.cz", "nslev21.cz", "komunistickastranacechamoravy", "RSCMAS", "cssdcz", "anobudelip", "moravaci", "ceskastranaregionu", "ods.cz", "VIZE2014", "hnutiusvit.cz", "stranazelenych", "svobodni", "rdscr", "ksc.cz", "Volte-pravý-blok-wwwcibulkanet/117925194908879", )
+library(Rfacebook)
 
-getPage("snked", token="fb_oauth")
+strany  <- c("snked", "narodnidemokracie", "kducsl", "StranaZdravehoRozumu", "top09cz", "stranales.cz", "nslev21.cz", "komunistickastranacechamoravy", "RSCMAS", "cssdcz", "anobudelip", "moravaci", "ceskastranaregionu", "ods.cz", "VIZE2014", "hnutiusvit.cz", "stranazelenych", "svobodni", "rdscr", "ksc.cz", "117925194908879", "Antibursik", "197466333678437", "ceska.piratska.strana", "1401399660101451", "korunaceska.cz", "nezavisliobcane", "ceskastrananarodnesocialni", "OKstrana1", "veciverejne", "149047641828242")
+
+posty  <- list()
+counter <- 1
+
+for (i in strany) {
+  posty[[counter]]  <- getPage(i, token=fb_oauth, 3000)
+  print(counter)
+  counter  <- counter + 1
+}
+
+
+
+
+
