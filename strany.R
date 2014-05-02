b65e544dc02a393e05312bd4c55ab6d156c168ae
Petr Kočí <petr.koci@gmail.com>
2014-05-02 13:13:10 +0200

    aktualizavaná data

--- strany.R
+++ strany.R
@@ -1,23 +1,36 @@
-library(Rfacebook)
+install.packages("data.table")
+library(data.table)
 
-strany  <- c("snked", "narodnidemokracie", "kducsl", "StranaZdravehoRozumu", "top09cz", "stranales.cz", "nslev21.cz", "komunistickastranacechamoravy", "RSCMAS", "cssdcz", "anobudelip", "moravaci", "ceskastranaregionu", "ods.cz", "VIZE2014", "hnutiusvit.cz", "stranazelenych", "svobodni", "rdscr", "ksc.cz", "117925194908879", "Antibursik", "197466333678437", "ceska.piratska.strana", "1401399660101451", "korunaceska.cz", "nezavisliobcane", "ceskastrananarodnesocialni", "OKstrana1", "veciverejne", "149047641828242")
+strany  <- c("snked", "narodnidemokracie", "kducsl", "StranaZdravehoRozumu", "top09cz", "stranales.cz", "nslev21.cz", "komunistickastranacechamoravy", "RSCMAS", "cssdcz", "anobudelip", "moravaci", "ceskastranaregionu", "ods.cz", "VIZE2014", "hnutiusvit.cz", "stranazelenych", "svobodni", "rdscr", "ksc.cz", "117925194908879", "Antibursik", "197466333678437", "ceska.piratska.strana", "1401399660101451", "korunaceska.cz", "aktivnezavislychobcanu", "ceskastrananarodnesocialni", "OKstrana1", "veciverejne", "149047641828242")
 
-posty  <- list()
+postyStrany  <- list()
 counter <- 1
 
-for (i in strany[24:31]) {
-  posty[[i]]  <- getPage(i, token=fb_oauth, 4000)
+for (i in strany[30:31]) {
+  postyStrany[[i]]  <- getPage(i, token=fb_oauth, 4000)
   print(i)
   counter  <- counter + 1
 }
 
 
-save(posty, file="posty")
+# filtruj data
+
+output  <- lapply(postyStrany, data.table)
+output  <- lapply(output, function(x) {x[as.Date(x$created_time) > as.Date("2012-01-01"), ]})
+
+
+# ulož data
+
+save(postyStrany, file="postyStrany.R")
+
+load("postyStrany.R")
+
 
 library(jsonlite)
-
 json  <- toJSON(posty)
-
 con  <- file("posty.json", "w")
 writeLines(json, con)
+close(con)
 
+
+postyStrany[[29]]
